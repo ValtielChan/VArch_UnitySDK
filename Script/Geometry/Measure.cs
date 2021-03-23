ccafb66d2b29333c28dd6ee929211f51819669a1
Fabien Boco <fabien.boco@gmail.com>
2021-03-23 17:50:32 +0100

    Add Tipbox

---
+++ Script/Geometry/Measure.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace VArch.SDK.Geometry
+{
+    public class Measure
+    {
+        public static float HorizontalDistance(Vector3 p1, Vector3 p2)
+        {
+            float dist = Vector2.Distance(new Vector2(p1.x, p1.z), new Vector2(p2.x, p2.z));
+            return dist;
+        }
+    }
+}
---
+++ Script/UI/Tipbox.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VArch.SDK.Utility;
+
+namespace VArch.SDK.Geometry
+{
+    public class Tipbox : MonoBehaviour
+    {
+        [SerializeField]
+        private GameObject tipboxUI;
+
+        [SerializeField]
+        private GameObject player;
+
+        [SerializeField]
+        private bool displayFromDistance;
+
+        [SerializeField]
+        private float displayDistance;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            DisplayTipbox(false);
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (displayFromDistance)
+            {
+                DisplayTipbox(Measure.HorizontalDistance(player.transform.position, transform.position) < displayDistance);
+            }
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.gameObject.tag.Equals("Controller"))
+            {
+                DisplayTipbox(true);
+            }
+        }
+
+        private void OnTriggerExit(Collider other)
+        {
+            if (other.gameObject.tag.Equals("Controller"))
+            {
+                DisplayTipbox(false);
+            }
+        }
+
+        private void DisplayTipbox(bool display)
+        {
+            tipboxUI.SetActive(display);
+        }
+
+        public void SetActive(bool active)
+        {
+            displayFromDistance = active;
+            tipboxUI.SetActive(active);
+        }
+    }
+}
